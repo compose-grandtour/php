865f08338de216471e2de5d3cd4b890f29c78c9c
Lorna Jane Mitchell <lorna@lornajane.net>
2018-06-19 14:23:36 +0100

    RabbitMQ connection fix and examples progress

--- example-rabbitmq/config.template.php
+++ example-rabbitmq/config.template.php
@@ -4,3 +4,26 @@
 
 $connection_string = 'amqps://username:password@server:port/path';
 
+// ** No need to edit below here **
+
+$url_bits = parse_url($connection_string);
+$rabbitmq['host'] = $url_bits['host'];
+$rabbitmq['port'] = $url_bits['port'];
+$rabbitmq['vhost'] = substr($url_bits['path'], 1);
+$rabbitmq['username'] = $url_bits['user'];
+$rabbitmq['password'] = $url_bits['pass'];
+
+$connection_ssl_options = [
+    "server_name_indication" => $rabbitmq['host']
+];
+
+$connection = new \PhpAmqpLib\Connection\AMQPSSLConnection(
+	$rabbitmq['host'],
+	$rabbitmq['port'],
+	$rabbitmq['username'],
+	$rabbitmq['password'],
+    $rabbitmq['vhost'],
+    $ssl_options
+);
+
+
--- example-rabbitmq/example-rabbitmq.php
+++ example-rabbitmq/example-rabbitmq.php
@@ -1,26 +1,9 @@
 <?php
 require('vendor/autoload.php'); // brings the amqplib dependency
-require('config.php'); // this sets $connection_string
-
-var_dump($connection_string);
-$url_pieces = parse_url($connection_string);
-
-$ssl_options = [];
-$connection = new \PhpAmqpLib\Connection\AMQPSSLConnection (
-    $url_pieces['host'],
-    $url_pieces['port'],
-    $url_pieces['user'],
-    $url_pieces['pass'],
-    $url_pieces['path'],
-    $ssl_options);
+require('config.php'); // this sets $connection
 
 $channel = $connection->channel();
 $channel->queue_declare('grand_tour');
-$message = ["word" => "hai", "type" => "greeting"];
-$amqp_msg = new PhpAmqpLib\Message\AMQPMessage(json_encode($message));
-$channel->basic_publish($amqp_msg, '', 'grand_tour');
-exit;
-
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     header('Content-type: application/json');
@@ -40,11 +23,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
     parse_str(file_get_contents("php://input"), $put_vars);
     if (isset($put_vars['word'])) {
-        $w = $put_vars['word'];
-        $d = $put_vars['definition'];
+        $word = $put_vars['word'];
+        $defn = $put_vars['definition'];
 
-        $redis->hmset('word:' . $w, ['word' => $w, 'definition' => $d]); 
+        $message = ["word" => $word, "type" => $defn];
+        $amqp_msg = new PhpAmqpLib\Message\AMQPMessage(json_encode($message));
+        $channel->basic_publish($amqp_msg, '', 'grand_tour');
 
-        echo 'added word ',$w,' and definition ',$d,' to database';
+        echo 'added word ',$word,' and definition ',$defn,' to queue';
     }
 }
